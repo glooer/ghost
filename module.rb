4120aa6d1777828a6daf8a81052f419b43fb285d
Konstantin P <kostya14@gmail.com>
2016-04-15 22:56:10 +0500

    Переписано 6 задание, теперь оно восхитительно.

--- module.rb
+++ module.rb
@@ -84,32 +84,10 @@ module Ghost
 =begin
   (6)
   Написать программу, которая позволяет сжимать текст
-  
-  криво :(
 =end
 
   def compress(str)
-    str = str.split("")
-    res = ""
-    cache = []
-    str.each do |v|
-      if cache.last == v
-        cache << v
-      else
-        if cache.size > 2 
-          res += "#{cache.last}(#{cache.size})"
-        else
-          res += cache.join
-        end
-        cache = [v]
-      end      
-    end
-    if cache.size > 2 
-      res += "#{cache.last}(#{cache.size})"
-    else
-      res += cache.join
-    end
-    res
+    str.gsub(/(.)\1{2,}/){ |v| "#{v[0]}(#{v.size})" }
   end
 
 =begin
